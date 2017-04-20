c638a57136fba296c5ac47b581d9526413717ccd
chenyu <1363696101@qq.com>
2017-04-20 10:15:52 +0800

    修改gridview的adapter

--- app/src/main/java/com/chy/videotutorial/module/main/view/CourseTitleGridViewAdapter.java
+++ app/src/main/java/com/chy/videotutorial/module/main/view/CourseTitleGridViewAdapter.java
@@ -37,7 +37,7 @@ public class CourseTitleGridViewAdapter extends BaseAdapter {
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
