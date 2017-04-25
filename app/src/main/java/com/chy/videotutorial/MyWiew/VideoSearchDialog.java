5d8c191971ffbbdf2515238e7e4c9d14b520b4b6
chenyu <1363696101@qq.com>
2017-04-25 18:01:43 +0800

    自定义Dialog

--- app/src/main/java/com/chy/videotutorial/MyWiew/VideoSearchDialog.java
+++ app/src/main/java/com/chy/videotutorial/MyWiew/VideoSearchDialog.java
@@ -3,8 +3,13 @@ package com.chy.videotutorial.MyWiew;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.chy.videotutorial.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 /**
@@ -12,19 +17,21 @@ import com.chy.videotutorial.R;
  */
 
 public class VideoSearchDialog extends Dialog {
+    private Context mContext;
+
+
     public VideoSearchDialog(Context context) {
         super(context, R.style.MyDialog);
+        mContext = context;
     }
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_video_search_layout);
+        ButterKnife.bind(this);
         //按空白处不能取消动画
-        setCanceledOnTouchOutside(false);
+//        setCanceledOnTouchOutside(false);
 
 //        //初始化界面控件
 //        initView();
@@ -34,4 +41,16 @@ public class VideoSearchDialog extends Dialog {
 //        initEvent();
 
     }
+
+    private void initView(){
+
+    }
+
+
+    @OnClick(R.id.btSearch)
+    public void onSearch(){
+        Toast.makeText(mContext,"111",Toast.LENGTH_SHORT).show();
+    }
+
+
 }
